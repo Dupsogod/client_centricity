0fd3c519e04bd6d9f0f2333a5b2c1ebe92660503
Dupsogod <dupsogod@gmail.com>
2020-07-07 11:07:05 +0300

    added popup

--- #src/js/popup.js
+++ #src/js/popup.js
@@ -1,11 +1,13 @@
 $('.popup-open').click(function () {
   var local_id = $(this).attr('data-tab');
+  $("body").css("overflow","hidden");
   $('#' + local_id).fadeIn();
   return false;
 });
 
 $('.popup-close').click(function () {
   $(this).parents('.popup-fade').fadeOut();
+  $("body").css("overflow","auto");
   return false;
 });
 
--- dist/js/script.js
+++ dist/js/script.js
@@ -3518,12 +3518,14 @@ $(window).scroll(function () {
  
 $('.popup-open').click(function () {
   var local_id = $(this).attr('data-tab');
+  $("body").css("overflow","hidden");
   $('#' + local_id).fadeIn();
   return false;
 });
 
 $('.popup-close').click(function () {
   $(this).parents('.popup-fade').fadeOut();
+  $("body").css("overflow","auto");
   return false;
 });
 
